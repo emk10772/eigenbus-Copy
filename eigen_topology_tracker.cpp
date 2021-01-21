a0d063ee7760be1b80341078e5b00cac7305cfed
nick-paiva <49162851+nick-paiva@users.noreply.github.com>
2021-01-21 14:10:21 -0500

    Support for port name tracking and depth tracking in topology code

--- eigen_topology_tracker.cpp
+++ eigen_topology_tracker.cpp
@@ -1,7 +1,8 @@
 #include "eigen_topology_tracker.h"
+#include <iterator>
 
 EigenTopologyTracker::EigenTopologyTracker(){
-
+    root_node_ = new Node(0xFF, this);
 }
 
 EigenTopologyTracker::~EigenTopologyTracker(){
@@ -29,11 +30,14 @@ void EigenTopologyTracker::process_updates(){
             if(node_map_.count(update->address()) != 0){
                 Node *node = node_map_[update->address()];
                 //Check if the node already has an entry for the specific port
+                eigen_addr_t ind = 0;
                 for(auto port : update->module()->downstream()){
-                    if(!node->has_child(port.addr_current)){
-                        Node *child = add_node(update->address(), node);
+                    if(port.addr_current != 0xFF && !node->has_child(port.addr_current)){
+                        Node *child = add_node(port.addr_current, node);
                         node->add_child(child);
+                        child->set_port(ind, port.name);
                     }
+                    ind++;
                 }
             }
         } else if (update->type() == EigenUpdate::MODULE_ADDED && update->module() != nullptr){
@@ -42,19 +46,19 @@ void EigenTopologyTracker::process_updates(){
             remove_node(update->address());
         }
 
-        it++;
         updates_.pop_front();
+        it = updates_.begin();
     }
 }
 
 EigenTopologyTracker::Node *EigenTopologyTracker::add_node(eigen_addr_t address, Node *parent){
     //If there is no node, make a new one
     if(node_map_.count(address) == 0){
-        Node *node = new Node(address, parent);
+        Node *node = new Node(address, this, parent);
         node_map_[address] = node;
 
         if(parent == nullptr)
-            root_nodes_[address] = node;
+            root_node_->add_child(node);
 
         return node;
     //If there is a node, detach it from its current parent
@@ -68,7 +72,7 @@ EigenTopologyTracker::Node *EigenTopologyTracker::add_node(eigen_addr_t address,
         }
 
         if(parent == nullptr)
-            root_nodes_[address] = node;
+            root_node_->add_child(node);
 
         return node;
     }
@@ -78,41 +82,78 @@ void EigenTopologyTracker::remove_node(eigen_addr_t address){
     if(node_map_.count(address) != 0){
         Node *node = node_map_[address];
 
-        if(node->parent() == nullptr)
-            root_nodes_.erase(address);
+        node->parent()->remove_child(node);
 
         node_map_.erase(address);
         for(auto child : node->children()){
-            child->set_parent(nullptr);
-            root_nodes_[address] = child;
+            root_node_->add_child(child.second);
         }
 
         delete node;
     }
 }
 
-eigen_addr_t EigenTopologyTracker::max_depth(){
-    return 0;
+const EigenTopologyTracker::Node *EigenTopologyTracker::get_node(eigen_addr_t address) const{
+    if(node_map_.count(address) > 0){
+        Node *node = node_map_.at(address);
+        return node;
+    } else if(address == root_node_->address()){
+        return root_node_;
+    }
+    return nullptr;
 }
 
-std::vector<eigen_addr_t> EigenTopologyTracker::addr_by_depth(eigen_addr_t node_depth){
-    return std::vector<eigen_addr_t>();
+const EigenTopologyTracker::Node *EigenTopologyTracker::root_node() const{
+    return root_node_;
 }
 
-std::vector<eigen_addr_t> EigenTopologyTracker::addr_by_downstream(eigen_addr_t addr, uint8_t port){
-    return std::vector<eigen_addr_t>();
+const std::vector<EigenTopologyTracker::Node *> EigenTopologyTracker::get_depth_list(eigen_addr_t depth) const{
+    if(depth < depth_list_.size())
+        return depth_list_[depth];
+    else
+        return std::vector<EigenTopologyTracker::Node *>();
+}
+
+eigen_addr_t EigenTopologyTracker::max_depth() const{
+    return depth_list_.size();
+}
+
+void EigenTopologyTracker::remove_depth(Node *node, eigen_addr_t depth){
+    if(depth >= depth_list_.size()) return;
+
+    auto it = depth_list_[depth].begin();
+    while(it != depth_list_[depth].end()){
+        if((*it)->address() == node->address())
+            it = depth_list_[depth].erase(it);
+        else
+            it++;
+    }
+
+    if(depth == (depth_list_.size() - 1) && depth_list_[depth].empty())
+        depth_list_.pop_back();
+}
+
+void EigenTopologyTracker::add_depth(Node *node, eigen_addr_t depth){
+    while(depth >= depth_list_.size()){
+        depth_list_.push_back(std::vector<Node *>());
+    }
+
+    depth_list_[depth].push_back(node);
 }
 
 
-
-EigenTopologyTracker::Node::Node(eigen_addr_t address){
+EigenTopologyTracker::Node::Node(eigen_addr_t address, EigenTopologyTracker *tracker){
     parent_ = nullptr;
     addr_ = address;
+    tracker_ = tracker;
+    depth_ = 0;
 }
 
-EigenTopologyTracker::Node::Node(eigen_addr_t address, EigenTopologyTracker::Node *parent){
+EigenTopologyTracker::Node::Node(eigen_addr_t address, EigenTopologyTracker *tracker, EigenTopologyTracker::Node *parent){
     parent_ = parent;
     addr_ = address;
+    tracker_ = tracker;
+    depth_ = 0;
 }
 
 EigenTopologyTracker::Node::~Node(){
@@ -120,30 +161,93 @@ EigenTopologyTracker::Node::~Node(){
         parent_->remove_child(this);
 }
 
-eigen_addr_t EigenTopologyTracker::Node::address(){
+eigen_addr_t EigenTopologyTracker::Node::address() const{
     return addr_;
 }
 
-EigenTopologyTracker::Node *EigenTopologyTracker::Node::parent(){
+eigen_addr_t EigenTopologyTracker::Node::port_id() const{
+    return port_id_;
+}
+
+std::string EigenTopologyTracker::Node::port_name() const{
+    return port_name_;
+}
+
+std::string EigenTopologyTracker::Node::text() const{
+    if(is_root())
+        return strprintf("Module %02X", addr_);
+    else
+        return strprintf("%01X: Module %02X", port_id_, addr_);
+}
+
+void EigenTopologyTracker::Node::set_port(eigen_addr_t id, std::string name){
+    port_id_ = id;
+    port_name_ = name;
+}
+
+const EigenTopologyTracker::Node *EigenTopologyTracker::Node::parent() const{
     return parent_;
 }
 
-std::vector<EigenTopologyTracker::Node *> EigenTopologyTracker::Node::children(){
+EigenTopologyTracker::Node *EigenTopologyTracker::Node::parent() {
+    return parent_;
+}
+
+int EigenTopologyTracker::Node::childNumber() const{
+    if(parent_)
+        return std::distance(parent_->children_.begin(), parent_->children_.find(addr_));
+    return 0;
+}
+
+const std::map<eigen_addr_t, EigenTopologyTracker::Node *> EigenTopologyTracker::Node::children() const{
     return children_;
+}
+
+const EigenTopologyTracker::Node *EigenTopologyTracker::Node::child(eigen_addr_t index) const{
+    if(index >= children_.size()) return nullptr;
+
+    auto it = children_.begin();
+    for(eigen_addr_t ind = 0; ind < index; ind++)
+        it++;
+
+    return it->second;
+}
+
+void EigenTopologyTracker::Node::update_depth(EigenTopologyTracker::Node *caller){
+    Node *caller_ = caller;
+    if(caller == nullptr) //First call
+        caller_ = this;
+    else if(caller == this) //Guard against loops in the graph
+        return;
+
+    tracker_->remove_depth(this, depth_);
+    //If we don't have a parent, our depth is 0
+    depth_ = (parent_) ? parent_->depth() + 1 : 0;
+    tracker_->add_depth(this, depth_);
+
+    //Update our children
+    for(auto child : children_){
+        child.second->update_depth(caller);
+    }
 }
 
 void EigenTopologyTracker::Node::set_parent(EigenTopologyTracker::Node *parent){
     parent_ = parent;
+
+    //Reset upstream info
+    port_id_ = 0xFF;
+    port_name_ = "";
+
+    //Recalculate the depth and the depth of the node's children
+    update_depth();
 }
 
 void EigenTopologyTracker::Node::add_child(EigenTopologyTracker::Node *child){
     //Make sure we don't have the child already
-    for(auto child_ : children_){
-        if(child_->address() == child->address())
-            return;
+    if(children_.count(child->address()) == 0){
+        children_[child->address()] = child;
+        child->set_parent(this);
     }
-
-    children_.push_back(child);
 }
 
 void EigenTopologyTracker::Node::remove_child(EigenTopologyTracker::Node *child){
@@ -151,26 +255,21 @@ void EigenTopologyTracker::Node::remove_child(EigenTopologyTracker::Node *child)
 }
 
 void EigenTopologyTracker::Node::remove_child(eigen_addr_t child_addr){
-    auto it = children_.begin();
-    while(it != children_.end()){
-        if(child_addr == (*it)->address())
-            it = children_.erase(it);
-        else
-            it++;
-    }
+    children_.erase(child_addr);
 }
 
-bool EigenTopologyTracker::Node::has_child(eigen_addr_t child){
-    auto it = children_.begin();
-    while(it != children_.end()){
-        if(child == (*it)->address())
-            return true;
-        else
-            it++;
-    }
-    return false;
+bool EigenTopologyTracker::Node::has_child(eigen_addr_t child) const{
+    return children_.count(child) > 0;
 }
 
-bool EigenTopologyTracker::Node::is_root(){
+bool EigenTopologyTracker::Node::is_root() const{
+    if(parent_ != nullptr)
+        return parent_->address() == 0xFF;
+
     return parent_ == nullptr;
 }
+
+eigen_addr_t EigenTopologyTracker::Node::depth() const{
+    return depth_;
+}
+
--- eigen_topology_tracker.h
+++ eigen_topology_tracker.h
@@ -18,39 +18,58 @@ public:
     void add_update(std::vector<EigenUpdate *>);
     void process_updates();
 
-    eigen_addr_t max_depth();
-    std::vector<eigen_addr_t> addr_by_depth(eigen_addr_t node_depth);
-    std::vector<eigen_addr_t> addr_by_downstream(eigen_addr_t addr, uint8_t port);
-private:
+    eigen_addr_t max_depth() const;
+
     class Node{
     public:
-        Node(eigen_addr_t address);
-        Node(eigen_addr_t address, Node *parent);
+        Node(eigen_addr_t address, EigenTopologyTracker *tracker);
+        Node(eigen_addr_t address, EigenTopologyTracker *tracker, Node *parent);
         ~Node();
 
-        eigen_addr_t address();
+        eigen_addr_t address() const;
+        eigen_addr_t port_id() const;
+        std::string port_name() const;
+        std::string text() const;
+        const Node *parent() const;
+        int childNumber() const;
         Node *parent();
-        std::vector<Node *> children();
+        const std::map<eigen_addr_t, Node *> children() const;
+        const Node *child(eigen_addr_t index) const;
 
+        void update_depth(Node *caller = nullptr);
         void set_parent(Node *parent);
+        void set_port(eigen_addr_t id, std::string name);
         void add_child(Node *child);
         void remove_child(Node *child);
         void remove_child(eigen_addr_t child_addr);
-        bool has_child(eigen_addr_t child);
-        bool is_root();
+        bool has_child(eigen_addr_t child) const;
+        bool is_root() const;
+        eigen_addr_t depth() const;
 
     private:
         eigen_addr_t addr_;
-        std::vector<Node *> children_;
+        std::map<eigen_addr_t, Node *> children_;
         Node *parent_;
+        eigen_addr_t port_id_;
+        std::string port_name_;
+        eigen_addr_t depth_;
+        EigenTopologyTracker *tracker_;
     };
 
+    const Node *get_node(eigen_addr_t address) const;
+    const Node *root_node() const;
+    const std::vector<Node *> get_depth_list(eigen_addr_t depth) const;
+
+private:
     Node *add_node(eigen_addr_t address, Node *parent = nullptr);
     void remove_node(eigen_addr_t address);
+    void remove_depth(Node *node, eigen_addr_t depth);
+    void add_depth(Node *node, eigen_addr_t depth);
 
     std::deque<EigenUpdate *> updates_;
-    std::map<eigen_addr_t, Node *> root_nodes_;
+    Node *root_node_;
     std::map<eigen_addr_t, Node *> node_map_;
+    std::vector<std::vector<Node *>> depth_list_;
 };
 
 #endif
